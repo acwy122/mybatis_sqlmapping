22c9682aad95923e4efbd61909ddb9f55eeccddc
xurb <xurongbiao@hmtim.com>
2022-05-20 22:07:58 +0800

    关联查询实例

--- src/main/java/com/mashibing/dao/EmpDao.java
+++ src/main/java/com/mashibing/dao/EmpDao.java
@@ -1,25 +1,21 @@
 package com.mashibing.dao;
 
 import com.mashibing.bean.Emp;
-import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-import java.util.Map;
-
 
 public interface EmpDao {
 
-//    @Insert()
-    public Integer save(Emp emp);
-//    @Update()
-    public Integer update(Emp emp);
-//    @Delete()
-    public Integer delete(Integer empno);
-//    @Select()
     public Emp selectEmpByEmpno(Integer empno);
 
-    public List<Emp> selectEmpList();
+    public Emp selectEmpByStep(Integer empno);
 
-    @MapKey("ename")
-    public Map<String, Emp> selectAll2();
+    public Emp selectEmpByStep2(Integer deptno);
+
+    public Emp selectEmpByCondition(Emp emp);
+
+    public List<Emp> selectEmpByDeptnos(@Param("deptnos") List<Integer> deptnos);
+
+    public Integer update(Emp emp);
 }
